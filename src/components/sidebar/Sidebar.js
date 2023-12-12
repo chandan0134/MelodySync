7c6dd2439fed0835e8908f7d8ddf68ceb0490012
chandan0134 <chandanma39@gmail.com>
2023-12-12 06:10:30 +0530

    library completed

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -14,18 +14,19 @@ function Sidebar() {
   const [image, setImage] = useState(
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdLAY3C19kL0nV2bI_plU3_YFCtra0dpsYkg&usqp=CAU"
   );
-
-  useEffect(()=>{
+  useEffect(() => {
     apiClient.get("me").then((response) => {
-      setImage(response.data.images.uri);
-      
-    });
+    //   setImage(response.data.images[0].url);
+    console.log(response);
+     });
+   
+  }, []);
 
-  },[]);
+ 
   return (
     <div className="sidebar-container">
       <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdLAY3C19kL0nV2bI_plU3_YFCtra0dpsYkg&usqp=CAU"
+        src={image}
         className="profile-image"
         alt=""
       />
--- src/screens/library/library.js
+++ src/screens/library/library.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import APIkit from '../../spotify';
+import './library.css';
+import { IconContext } from "react-icons";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 
 function Library() {
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    // Fetch user's playlists
+    APIkit.get('me/playlists').then(function (response) {
+      // Access the items array from response.data
+      setPlaylists(response.data.items);
+    });
+  }, []);
+
+  const navigate = useNavigate();
+
+  const playPlaylist = (id) => {
+    navigate("/player", { state: { id: id } });
+  };
+
   return (
-    <div className='screen-container'>HDVBV</div>
-  )
+    <div className='screen-container'>
+      <div className='library-body'>
+      {playlists?.map((playlist) => (
+        <div className="playlist-card" key={playlist.id} onClick={() => playPlaylist(playlist.id)}>
+        <img src={playlist.images[0].url} className="playlist-image" alt="playlist-Art" />
+         <p className="playlist-title">{playlist.name}</p>
+         <p className="playlist-subtitle">{playlist.tracks.total}</p>
+         <div className="playlist-fade">
+              <IconContext.Provider value={{ size: "50px", color: "#E99D72" }}>
+                <AiFillPlayCircle />
+              </IconContext.Provider>
+            </div>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
 }
 
-export default Library
+export default Library;
